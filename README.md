eff6ac1799ed507a748611cfdd60985c7365112c
Valentin Harapu <aharapu2@yahoo.com>
2023-01-20 18:06:54 +0200

    add postgres client

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # test-server
+
+## To see app engine service logs
+
+    ```bash
+    gcloud app logs tail -s <service name>
+    ```
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const { Client } = require("pg");
+
+// TODO -> test adding cors
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const pgClient = new Client();
+
+app.get("/users", async (req, res) => {
+  await pgClient.connect();
+
+  const res = await pgClient.query("SELECT * FROM users");
+  await pgClient.end();
+
+  res.send(res);
+});
+
+app.get("/josn-test", async (req, res) => {
+  res.send({ test: "test" });
 });
 
 app.listen(port, () => {
