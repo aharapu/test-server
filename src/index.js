14289a05aa5e061d43022704787b98eec8bc23f0
Valentin Harapu <aharapu2@yahoo.com>
2023-01-21 09:17:00 +0200

    add nodemon

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const { Client } = require("pg");
+require("dotenv").config();
 
 // TODO -> test adding cors
 const app = express();
 const port = 3000;
 
-const pgClient = new Client();
+const pgClient = new Client({
+  user: process.env.PG_VM_USER,
+  host: process.env.PG_VM_HOST,
+  database: process.env.PG_VM_DATABASE,
+  password: process.env.PG_VM_PASSWORD,
+  port: process.env.PG_VM_PORT,
+});
 
 app.get("/users", async (req, res) => {
   await pgClient.connect();
 
-  const res = await pgClient.query("SELECT * FROM users");
+  const result = await pgClient.query("SELECT * FROM users");
   await pgClient.end();
 
-  res.send(res);
+  res.send(result);
 });
 
-app.get("/josn-test", async (req, res) => {
+app.get("/json-test", async (req, res) => {
   res.send({ test: "test" });
 });
 
